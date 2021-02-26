4348f88d29d3161ce9b34db4fadcdfe180fdfdfd
PureBlackSoftware <PureBlackSoftware@users.noreply.github.com>
2021-02-26 17:03:58 +0300

    1.0

--- app/src/main/java/com/pureblacksoft/gradinfo/activity/MainActivity.kt
+++ app/src/main/java/com/pureblacksoft/gradinfo/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pureblacksoft.gradinfo.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,10 @@ import com.pureblacksoft.gradinfo.service.GradDataService
 
 class MainActivity : AppCompatActivity()
 {
+    companion object {
+        var onSuccessfulService: (() -> Unit)? = null
+    }
+
     lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
@@ -31,9 +36,23 @@ class MainActivity : AppCompatActivity()
         //endregion
 
         startGradDataService()
+
+        //region Events
+        GradDataService.onSuccess = {
+            binding.spinKitMA.visibility = View.GONE
+
+            onSuccessfulService?.invoke()
+        }
+
+        GradDataService.onFailure = {
+            binding.spinKitMA.visibility = View.GONE
+        }
+        //endregion
     }
 
     private fun startGradDataService() {
+        binding.spinKitMA.visibility = View.VISIBLE
+
         val intent = Intent(this, GradDataService::class.java)
         GradDataService.enqueueWork(this, intent)
     }
--- app/src/main/java/com/pureblacksoft/gradinfo/fragment/HomeFragment.kt
+++ app/src/main/java/com/pureblacksoft/gradinfo/fragment/HomeFragment.kt
@@ -66,7 +66,7 @@ class HomeFragment : Fragment(R.layout.fragment_home)
         //endregion
 
         //region Events
-        GradDataService.onSuccess = {
+        MainActivity.onSuccessfulService = {
             setGradAdapter()
         }
         //endregion
@@ -86,7 +86,7 @@ class HomeFragment : Fragment(R.layout.fragment_home)
     }
 
     private fun setGradAdapter() {
-        if (GradDataService.gradList.size != 0) {
+        if (_binding != null && GradDataService.gradList.size != 0) {
             Log.d(TAG, "setGradAdapter: Running")
 
             gradAdapter = GradAdapter(GradDataService.gradList)
