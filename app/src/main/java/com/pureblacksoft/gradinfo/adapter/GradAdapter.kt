600a868163272279dec0c57ef9bf8872413dfcf0
PureBlackSoftware <PureBlackSoftware@users.noreply.github.com>
2021-02-28 23:28:59 +0300

    1.0

--- app/src/main/java/com/pureblacksoft/gradinfo/adapter/GradAdapter.kt
+++ app/src/main/java/com/pureblacksoft/gradinfo/adapter/GradAdapter.kt
@@ -3,14 +3,18 @@ package com.pureblacksoft.gradinfo.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.pureblacksoft.gradinfo.data.Grad
 import com.pureblacksoft.gradinfo.databinding.CardGradBinding
+import java.util.*
 
-class GradAdapter(private val gradList: MutableList<Grad>) : RecyclerView.Adapter<GradAdapter.ViewHolder>()
+class GradAdapter(private val gradList: MutableList<Grad>) : RecyclerView.Adapter<GradAdapter.ViewHolder>(), Filterable
 {
     private lateinit var context: Context
+    private var filteredGradList = gradList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
@@ -22,11 +26,42 @@ class GradAdapter(private val gradList: MutableList<Grad>) : RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val grad = gradList[position]
+        val grad = filteredGradList[position]
         holder.bind(grad)
     }
 
-    override fun getItemCount(): Int = gradList.size
+    override fun getItemCount(): Int = filteredGradList.size
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val resultList = mutableListOf<Grad>()
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    resultList.clear()
+                } else {
+                    for (grad in gradList) {
+                        if (grad.name.toLowerCase(Locale.ROOT).contains(charSearch.toLowerCase(
+                                Locale.ROOT))) {
+                            resultList.add(grad)
+                        }
+                    }
+                }
+                filteredGradList = resultList
+
+                val filterResults = FilterResults()
+                filterResults.values = filteredGradList
+
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredGradList = results?.values as MutableList<Grad>
+                notifyDataSetChanged()
+            }
+        }
+    }
 
     inner class ViewHolder(private val binding: CardGradBinding) : RecyclerView.ViewHolder(binding.root)
     {
