8059bed04c7c16d59af19d4a7fd74660d2633158
PureBlackSoftware <PureBlackSoftware@users.noreply.github.com>
2021-03-03 17:53:24 +0300

    1.0

--- app/src/main/java/com/pureblacksoft/gradinfo/activity/PrefActivity.kt
+++ app/src/main/java/com/pureblacksoft/gradinfo/activity/PrefActivity.kt
@@ -2,12 +2,57 @@ package com.pureblacksoft.gradinfo.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.pureblacksoft.gradinfo.R
+import com.pureblacksoft.gradinfo.databinding.ActivityPrefBinding
 
 class PrefActivity : AppCompatActivity()
 {
+    companion object {
+        private const val ID_THEME_DEFAULT = 0
+        private const val ID_THEME_LIGHT = 1
+        private const val ID_THEME_DARK = 2
+
+        private var currentTheme = ID_THEME_DEFAULT
+    }
+
+    private lateinit var binding: ActivityPrefBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pref)
+
+        binding = ActivityPrefBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkCurrentTheme()
+
+        //region Buttons
+        binding.imgToolbarBackPA.setOnClickListener {
+            onBackPressed()
+        }
+
+        binding.rltDefaultThemePA.setOnClickListener {
+            currentTheme = ID_THEME_DEFAULT
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+
+        binding.rltLightThemePA.setOnClickListener {
+            currentTheme = ID_THEME_LIGHT
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        binding.rltDarkThemePA.setOnClickListener {
+            currentTheme = ID_THEME_DARK
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        //endregion
+    }
+
+    private fun checkCurrentTheme() {
+        when (currentTheme) {
+            ID_THEME_DEFAULT -> binding.imgDefaultThemePA.setImageResource(R.drawable.ic_check_v2)
+            ID_THEME_LIGHT -> binding.imgLightThemePA.setImageResource(R.drawable.ic_check_v2)
+            ID_THEME_DARK -> binding.imgDarkThemePA.setImageResource(R.drawable.ic_check_v2)
+        }
     }
 }
